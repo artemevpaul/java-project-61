16eba3c09d6e3882bff536d67891eb0607433a02
Pavel Artemev <damention@Pavels-MacBook-Pro-2.local>
2024-02-02 23:18:57 +0100

    linter fixes

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,8 @@
 package hexlet.code;
 
-import hexlet.code.games.Progression;
-
 import java.util.Scanner;
 
 public class App {
-
     public static String gameName = "";
     public static void main(String[] args){
         System.out.print("Welcome to the Brain Games! \n");
@@ -14,7 +11,7 @@ public class App {
         System.out.print("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit \n");
         System.out.println("Your choice: ");
         gameName = scanner.next();
-        if(gameName.equals("1")) {
+        if (gameName.equals("1")) {
             Cli.greet();
         } else if (!gameName.equals("0")) {
             Engine.gameLoop();
--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -2,20 +2,21 @@ package hexlet.code.games;
 
 import hexlet.code.Engine;
 
-//import java.util.Arrays;
 import java.util.Scanner;
 
 public class Progression {
-    public static void generator(){
-        int maxProgLength = 10, minProgLength = 5;
+    public static void generator() {
+        int maxProgLength = 10;
+        int minProgLength = 5;
         int progressionLength = minProgLength + (int) (Math.random() * ((maxProgLength - minProgLength) + 1));
         var newProgression = new String[progressionLength];
         var progressionBlankElement = (int) (Math.random() * progressionLength);
         var progressionDiff = (int) (Math.random() * progressionLength);
-        var progressionStart = String.valueOf(minProgLength + (int) (Math.random() * ((maxProgLength - minProgLength) + 1)));
+        var progressionStart = String.valueOf(minProgLength +
+                (int) (Math.random() * ((maxProgLength - minProgLength) + 1)));
         newProgression[0] = progressionStart;
-        for (var i = 1; i < progressionLength; i++){
-            newProgression[i] = String.valueOf(Integer.parseInt(newProgression[i-1]) + progressionDiff);
+        for (var i = 1; i < progressionLength; i++) {
+            newProgression[i] = String.valueOf(Integer.parseInt(newProgression[i - 1]) + progressionDiff);
         }
         var correctAnswer = newProgression[progressionBlankElement];
         newProgression[progressionBlankElement] = "..";
@@ -35,7 +36,8 @@ public class Progression {
             Engine.roundResult = "win";
             Engine.count++;
         } else {
-            System.out.println("'" + missingNumber + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'");
+            System.out.println("'" + missingNumber + "' is wrong answer ;(. Correct answer was '"
+                    + correctAnswer + "'");
             Engine.roundResult = "lost";
             Engine.count = 0;
         }
