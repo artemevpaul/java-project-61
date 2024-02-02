28acb5b53ec4f0c3a3bc710d6c95d815266b1bc0
Pavel Artemev <damention@Pavels-MacBook-Pro-2.local>
2024-02-02 22:46:49 +0100

    fixed app

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,6 +8,7 @@ public class App {
 
     public static String gameName = "";
     public static void main(String[] args){
+        System.out.print("Welcome to the Brain Games! \n");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter the game number and press Enter. \n");
         System.out.print("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit \n");
